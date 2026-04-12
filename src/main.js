82d7e05c3a607e153e6bf518d46b14855a05624e
Tato <italoluna23@gmail.com>
2026-04-12 18:54:09 -0500

    add vee-validate rules for required and email validation in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { defineRule } from 'vee-validate'
+
+// Regla required
+defineRule('required', (value) => {
+  if (!value || !value.length) return 'This field is required'
+  return true
+})
+
+// Regla email
+defineRule('email', (value) => {
+  if (!value || !value.length) return true
+  if (!/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i.test(value)) {
+    return 'This field must be a valid email'
+  }
+  return true
+})
+
 createApp(App).mount('#app')
