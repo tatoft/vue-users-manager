74cc481c8e0a394690f5b0a03cf8656ba6514810
Tato <italoluna23@gmail.com>
2026-04-12 01:17:07 -0500

    add User entity class with constructor

---
+++ src/users/model/user.entity.js
@@ -0,0 +1,9 @@
+export class User {
+  constructor(id = '', name = '', username = '', email = '', phone = '') {
+    this.id = id
+    this.name = name
+    this.username = username
+    this.email = email
+    this.phone = phone
+  }
+}
